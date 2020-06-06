70dd1f89a41b072a11c54b2c582c4f8099114eb2
Muyinda ROgers <moverr@gmail.com>
2020-06-06 20:35:12 +0300

    Greetings Controller

--- src/main/java/com/kodeinc/producer/demo/DemoApplication.java
+++ src/main/java/com/kodeinc/producer/demo/DemoApplication.java
@@ -2,9 +2,17 @@ package com.kodeinc.producer.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DemoApplication {
+    
+    @PostMapping("greet/{name}")
+    public void publish(String name){
+        String greeting "Hello "+name;
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
