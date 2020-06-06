f144baf173c2932f1bd256d50b95d672a47efbf4
Muyinda ROgers <moverr@gmail.com>
2020-06-06 20:56:25 +0300

    :Debuggging :

--- src/main/java/com/kodeinc/producer/demo/DemoApplication.java
+++ src/main/java/com/kodeinc/producer/demo/DemoApplication.java
@@ -1,16 +1,29 @@
 package com.kodeinc.producer.demo;
 
+import java.util.function.Consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.messaging.MessageChannel;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@EnableBinding(ProducerChannels.class)
 @SpringBootApplication
 public class DemoApplication {
     
-    @PostMapping("greet/{name}")
+    
+    private final MessageChannel Consumer;
+
+    public DemoApplication(ProducerChannels channels) {
+        this.Consumer = channels.consumer();
+    }
+    
+    
+    @PostMapping("greet/{name}");
     public void publish(@PathVariable String name){
         String greeting =  "Hello "+name;
     }
@@ -19,4 +32,10 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+}
+
+interface ProducerChannels{
+    
+    @Output
+    MessageChannel consumer();
 }
